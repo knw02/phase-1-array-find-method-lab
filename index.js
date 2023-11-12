a4ee54cc13acbd2395e849703cdb9adaddd1a268
knw02 <147000437+knw02@users.noreply.github.com>
2023-11-12 18:14:05 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(superbowls) {
+    const winningSuperbowl = superbowls.find(superbowl => superbowl.result === "W");
+  
+    if (winningSuperbowl) {
+      return winningSuperbowl.year;
+    } else {
+      return undefined;
+    }
+  }
+  const superbowls = [
+    { year: 2019, result: "L" },
+    { year: 2020, result: "W" },
+    { year: 2021, result: "L" },
+  ];
